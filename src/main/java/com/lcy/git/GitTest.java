c1fcdbc931de8725308b75ec4c868e844badc3b2
Yu <lvchangyu529@163.com>
2022-06-19 16:45:37 +0800

    idea second commit

--- src/main/java/com/lcy/git/GitTest.java
+++ src/main/java/com/lcy/git/GitTest.java
@@ -3,5 +3,6 @@ package com.lcy.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("HelloGit");
+        System.out.println("HelloGit02");
     }
 }
